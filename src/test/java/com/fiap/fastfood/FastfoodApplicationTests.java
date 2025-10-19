7b2035fb9c0d1c14ba1b4f7d6c08ed260413d244
Kamilla Custódio <kamilla.custodio@gmail.com>
2025-10-19 17:58:48 -0300

    use db in memory to run tests

--- src/test/java/com/fiap/fastfood/FastfoodApplicationTests.java
+++ src/test/java/com/fiap/fastfood/FastfoodApplicationTests.java
@@ -2,8 +2,10 @@ package com.fiap.fastfood;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FastfoodApplicationTests {
 
     @Test
